11600818c2b673412c60ec2d167d04f28c6b9b53
Shirley <shirleyfrassa@gmail.com>
2021-09-01 15:50:27 -0300

    Se agrega clase Itinerario

--- src/usuario/Itinerario.java
+++ src/usuario/Itinerario.java
@@ -1,12 +1,43 @@
 package usuario;
 
+import java.util.Arrays;
+
 import atraccion.Sugerencia;
 
 public class Itinerario {
-
-	public void agregarCompra(Sugerencia sugerencia) {
-		// TODO Auto-generated method stub
-		
+	private Sugerencia[] comprasRealizadas = new Sugerencia[10];
+	private int posicionACargar = 0;
+	
+	public void agregarLaCompraDe(Sugerencia sugerencia) {
+		comprasRealizadas[posicionACargar] = sugerencia;
+		posicionACargar++;
+		//comprasRealizadas[posicionACargar++] = sugerencia;
 	}
+	
+	private int getPrecioTotalDeCompras() {
+		int precioTotal = 0;
+		for (int i = 0; i < comprasRealizadas.length; i++) {
+			if (comprasRealizadas[i] != null)
+				precioTotal += comprasRealizadas[i].getPrecio();
+		}
+		return precioTotal;
+	}
+	
+	private double getTiempoTotalDeCompras() {
+		double tiempoTotal = 0;
+		for (int i = 0; i < comprasRealizadas.length; i++) {
+			if (comprasRealizadas[i] != null)
+				tiempoTotal += comprasRealizadas[i].getDuracion();
+		}
+		return tiempoTotal;
+	}
+	
+	//pensar como fusionar los métodos anteriores que son iguales
+	
+	@Override
+	public String toString() {
+		return "Itinerario de las compras realizadas:" + "\n" + Arrays.toString(comprasRealizadas) + "\n" + "Gasto total: " + getPrecioTotalDeCompras() + ". Duración total: " + getTiempoTotalDeCompras();
+	}
+	
 
 }
