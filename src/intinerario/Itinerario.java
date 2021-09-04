d7aec8bfeaad9b319acda1c28b67d136bc0dd856
Shirley <shirleyfrassa@gmail.com>
2021-09-04 17:20:32 -0300

    Se agrega clase Itinerario completa

---
+++ src/intinerario/Itinerario.java
@@ -0,0 +1,36 @@
+package intinerario;
+
+import java.util.ArrayList;
+
+import sugerencia.Sugerencia;
+
+public class Itinerario {
+	private ArrayList<Sugerencia> comprasRealizadas = new ArrayList<Sugerencia>();
+	
+	public void agregarLaCompraDe(Sugerencia sugerencia) {
+		comprasRealizadas.add(sugerencia);
+	}
+	
+	private int getPrecioTotalDeCompras() {
+		int precioTotal = 0;
+		for (int i = 0; i < comprasRealizadas.size(); i++) {
+			precioTotal += comprasRealizadas.get(i).getPrecio();
+		}
+		return precioTotal;
+	}
+	
+	private double getTiempoTotalDeCompras() {
+		double tiempoTotal = 0;
+		for (int i = 0; i < comprasRealizadas.size(); i++) {
+			tiempoTotal += comprasRealizadas.get(i).getDuracion();
+		}
+		return tiempoTotal;
+	}
+	
+	//pensar como fusionar los métodos anteriores que son iguales
+	
+	@Override
+	public String toString() {
+		return "Itinerario de las compras realizadas:" + "\n" + comprasRealizadas.toString() + "\n" + "Gasto total: " + getPrecioTotalDeCompras() + ". Duración total: " + getTiempoTotalDeCompras();
+	}
+}
