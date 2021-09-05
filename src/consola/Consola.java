33e05198d288a3815d3e1bcbf3dd797105126616
Shirley <shirleyfrassa@gmail.com>
2021-09-05 11:22:19 -0300

    Se mejora método para mostrar sugerencias a los usuarios

--- src/consola/Consola.java
+++ src/consola/Consola.java
@@ -7,12 +7,12 @@ import java.util.Scanner;
 import intinerario.Itinerario;
 import ordenar.Ordenar;
 import sugerencia.Sugerencia;
+import tierraMedia.TierraMedia;
 import usuario.Usuario;
 
 public class Consola {
-	//cambiar usuarios y sugerencias con lo que viene por TierraMedia
-	private ArrayList<Usuario> usuarios = new ArrayList<Usuario>();
-	private ArrayList<Sugerencia> sugerencias = new ArrayList<Sugerencia>();
+	private ArrayList<Usuario> usuarios = TierraMedia.getUsuarios();
+	private ArrayList<Sugerencia> sugerencias = TierraMedia.getSugerencias();
 	private Scanner entrada = new Scanner(System.in);
 	private Itinerario itinerario;
 
@@ -22,17 +22,15 @@ public class Consola {
 
 			for (Sugerencia sugerencia : seleccionarSugerenciasPara(usuario)) {
 				System.out.println(sugerencia.toString());
-				String seleccion;
+				String seleccionDelUsuario;
 				do {
 					System.out.println("¿Desea elegir esta sugencia? (Presione 's' por SI o 'n' por NO");
-					seleccion = entrada.nextLine();
-				} while (seleccion != "s" || seleccion != "n");
+					seleccionDelUsuario = entrada.nextLine();
+				} while (seleccionDelUsuario != "s" || seleccionDelUsuario != "n");
 
-				if (seleccion == "s") {
+				if (seleccionDelUsuario == "s") {
 					this.itinerario.agregarLaCompraDe(sugerencia);
-					System.out.println("¡Compra exitosa!");
-					System.out.println("Dinero disponible: " + usuario.getDineroDisponible()
-							+ " monedas de oro. Tiempo disponible: " + usuario.getTiempoDisponible() + " horas.");
+					System.out.println(mensajeDeCompraExitosa(usuario));
 				}
 			}
 		}
@@ -44,16 +42,29 @@ public class Consola {
 				+ "Seleccione alguna de las siguientes sugerencias para disfutar su día en Tierra Media: " + "\n";
 	}
 
+	private String mensajeDeCompraExitosa(Usuario usuario) {
+		return "¡Compra exitosa!" + "\n" + "Dinero disponible: " + usuario.getDineroDisponible()
+				+ " monedas de oro. Tiempo disponible: " + usuario.getTiempoDisponible() + " horas.";
+	}
+
 	private ArrayList<Sugerencia> seleccionarSugerenciasPara(Usuario usuario) {
 		ArrayList<Sugerencia> sugerenciasAimprimir = new ArrayList<Sugerencia>();
 		for (Sugerencia sugerencia : sugerencias) {
-			if (sugerencia.getCupo() > 0 && sugerencia.getPrecio() <= usuario.getDineroDisponible()
-					&& sugerencia.getDuracion() <= usuario.getTiempoDisponible()) {
+			if (puedeSerOfrecidaLa(sugerencia) && elUsuarioPuedeComprar(usuario, sugerencia)) {
 				sugerenciasAimprimir.add(sugerencia);
 			}
 		}
-		Collections.sort(sugerenciasAimprimir, new Ordenar(usuario.getPreferencia()));
+		Collections.sort(sugerenciasAimprimir, new Ordenar(usuario.getPreferencia())); // Esto lo hace TierraMedia
 		return sugerenciasAimprimir;
+	}
+
+	private boolean puedeSerOfrecidaLa(Sugerencia sugerencia) {
+		return !this.itinerario.getComprasRealizadas().contains(sugerencia) && sugerencia.getCupo() > 0;
+	}
+
+	private boolean elUsuarioPuedeComprar(Usuario usuario, Sugerencia sugerencia) {
+		return sugerencia.getPrecio() <= usuario.getDineroDisponible()
+				&& sugerencia.getDuracion() <= usuario.getTiempoDisponible();
 	}
 
 }
